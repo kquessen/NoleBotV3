5228d3d82fec5368af0fdf67f414956aa74d0b77
kquessen <kmq22a@fsu.edu>
2025-04-27 01:11:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+TOKEN = os.getenv('MTM2NTkxMTM5ODY3NjI5OTg3OA.GqFAu1.otnwwZyAq5LeM8SuPeRkhsmkej0eb5-Qcmjx0U')
+
+# Configure bot intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# Create bot instance
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == '!ping':
+        await message.channel.send('Pong!')
+
+client.run(TOKEN)
 
